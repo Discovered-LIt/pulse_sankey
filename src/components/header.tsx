cba44c8853b7390931e3d65607dd3a68d2d4cd72
Bilal1122 <bilal-tariq@live.com>
2023-09-07 15:33:12 +0800

    fix: add z-index to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full h-16 bg-black fixed" />
+    <div className="w-full h-16 bg-black fixed z-10" />
   )
 }
 
