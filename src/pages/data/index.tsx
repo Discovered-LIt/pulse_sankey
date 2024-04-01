9a031e2c2654d18cbc2cb382b6fc4acd28ef9774
Bilal1122 <bilal-tariq@live.com>
2024-04-02 01:15:08 +0800

    fix: data page content hight

--- src/pages/data/index.tsx
+++ src/pages/data/index.tsx
@@ -154,7 +154,7 @@ const DataPage = () => {
         setFilters={setFilters}
         chartTypeOptions={chartTypeOptions}
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 max-h-[87vh] overflow-scroll gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 max-h-[70vh] overflow-scroll gap-4">
         {
           filteredCharts?.map((data) => (
             <div
