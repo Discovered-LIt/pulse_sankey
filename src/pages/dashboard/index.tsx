6887496a5c46055d940dd287408afe0ce139c263
Bilal1122 <bilal-tariq@live.com>
2024-05-19 15:12:40 +0500

    feat: change dashboard layout

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import cn from 'classnames';
 import { useNavigate } from "react-router-dom";
 // context
@@ -6,22 +6,28 @@ import { useTopicSettingsContext, Topic } from "../../context/TopicSettingsConte
 
 type Item = { label: string, logo: string, url?: string, param?: string; };
 
-const items: {[key: string]: Item[]} = {
-  "PEOPLE": [
-    { label: 'Stephen Curry', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/stephcurry_logo.svg', url: '/data', param: 'curry' },
-    { label: 'Taylor Swift', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/taylorswift_logo.svg' },
-    { label: 'Elon Musk', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/elon_logo.svg' },
-  ],
-  "BUSINESS": [
+enum Types {
+  COMPANIES = 'COMPANIES',
+  PEOPLE = 'PEOPLE'
+}
+
+const items: {[key in Types]: Item[]} = {
+  [Types.COMPANIES]: [
     { label: 'Tesla', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/Tesla_logo.svg', url: '/sankey' },
     { label: 'Meta', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/meta_logo.svg' },
     { label: 'Nvidia', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/nvidia_logo.svg' },
+  ],
+  [Types.PEOPLE]: [
+    { label: 'Stephen Curry', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/stephcurry_logo.svg', url: '/data', param: 'curry' },
+    { label: 'Taylor Swift', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/taylorswift_logo.svg' },
+    { label: 'Elon Musk', logo: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/elon_logo.svg' },
   ]
 };
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const { setActiveTopic } =  useTopicSettingsContext();
+  const [activeType, setActiveType] = useState<Types>(Types.COMPANIES);
 
   const onClick = (url: string, topic: string = '') => {
     if(!url) return;
@@ -32,32 +38,35 @@ const Dashboard = () => {
 
   return(
     <div>
-      <h1 className="mb-10 text-center mt-10">TOPICS</h1>
-      <div className="max-w-[600px] m-auto">
+      <div className="max-w-[600px] m-auto my-20 text-center">
         {
           Object.keys(items).map((key) => (
-            <div className="mb-14">
-              <h1 className="mb-8 font-bold sm:text-left text-center">{key}</h1>
-              <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                {items[key].map((item) => (
-                  <div
-                    className={cn([
-                      'p-2',
-                      item.url ? 'hover:bg-white cursor-pointer hover:text-black' : 'cursor-not-allowed'
-                    ])}
-                    onClick={() => onClick(item.url, item.param)}
-                  >
-                    <img
-                      src={item.logo}
-                      className='w-[100px] h-[120px] mb-4 m-auto'
-                    />
-                    <div className="mx-2 text-center">{item.label}</div>
-                  </div>
-                ))}
-              </div>
-            </div>
+            <button
+              className={cn([
+                "border-2 rounded-xl px-4 py-[4px] text-sm first:mr-14",
+                activeType === key ? 'border-gray-700' : 'border-transparent'
+              ])}
+              onClick={() => setActiveType(key as Types)}
+            >{key}</button>
           ))
         }
+        <div className="grid grid-cols-2 w-[300px] m-auto mt-14">
+          {items[activeType].map((item) => (
+            <div
+              className={cn([
+                'p-2 mb-4',
+                item.url ? 'hover:bg-white cursor-pointer hover:text-black' : 'cursor-not-allowed'
+              ])}
+              onClick={() => onClick(item.url, item.param)}
+            >
+              <img
+                src={item.logo}
+                className='w-[80px] h-[100px] mb-4 m-auto'
+              />
+              <div className="mx-2 text-center">{item.label}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
