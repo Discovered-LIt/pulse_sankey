bd0bdba2e8b4d6f067c49898c277e8c58ce71e99
tonymoo90 <tonymoo90@gmail.com>
2024-06-12 15:52:08 -0500

    Update index.tsx

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -57,8 +57,8 @@ const Dashboard = () => {
           {items[activeType].map((item) => (
             <div
               className={cn([
-                'p-2 mb-4 w-[200px] m-auto relative border-2 border-gray-700 rounded-xl',
-                item.url ? 'hover:border-white hover:bg-opacity-40 hover:bg-black cursor-pointer hover:text-black' : 'cursor-not-allowed'
+                'p-2 mb-4 w-[200px] m-auto relative border-2 border-transparent rounded-xl',
+                item.url ? 'hover:border-gray-700 hover:bg-opacity-40 hover:bg-black cursor-pointer hover:text-black' : 'cursor-not-allowed'
               ])}
               onClick={() => onClick(item.url, item.param)}
             >
