f54666f93241fd259e91ddcf27cddfecfbe49747
tonymoo90 <tonymoo90@gmail.com>
2024-06-17 06:16:38 -0500

    Update TopicSettingsContext.tsx

--- src/context/TopicSettingsContext.tsx
+++ src/context/TopicSettingsContext.tsx
@@ -10,6 +10,13 @@ export enum Topic {
   Nvidia = 'nvidia',
   Curry = 'curry',
   Meta = 'meta',
+  Apple = 'apple',
+  Microsoft = 'microsoft',
+  Alphabet = 'alphabet',
+  Netflix = 'netflix',
+  Amazon = 'amazon',
+  Snowflake = 'snowflake',
+  Celsius = 'celsius',
   Default = 'default'
 }
 
@@ -42,13 +49,55 @@ const settings: {[key in Topic]: Setting } = {
     theme: { primary: 'black', secondary: '' },
     logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/metalogo.svg"
   },
-  curry: {
+    curry: {
     datamappingUrl: '/steph/datamapping_steph.json',
     tabMenu: ['data'],
     theme: { primary: '#FFC214', secondary: '#0000FF' },
     logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/steph_pagelogo.svg"
   },
-  default: {
+    apple: {
+    datamappingUrl: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/AAPL.US/AAPL.US_metrics_mapping.json',
+    tabMenu: ['data'],
+    theme: { primary: 'black', secondary: '' },
+    logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/applelong.svg"
+  },
+    microsoft: {
+    datamappingUrl: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/MSFT.US/MSFT.US_metrics_mapping.json',
+    tabMenu: ['data'],
+    theme: { primary: 'black', secondary: '' },
+    logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/microsoftlong.svg"
+  },
+    alphabet: {
+    datamappingUrl: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/GOOG.US/GOOG.US_metrics_mapping.json',
+    tabMenu: ['data'],
+    theme: { primary: 'black', secondary: '' },
+    logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/alphabetlong.svg"
+  },
+    netflix: {
+    datamappingUrl: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/NFLX.US/NFLX.US_metrics_mapping.json',
+    tabMenu: ['data'],
+    theme: { primary: 'black', secondary: '' },
+    logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/netflixlong.svg"
+  },
+    amazon: {
+    datamappingUrl: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/AMZN.US/AMZN.US_metrics_mapping.json',
+    tabMenu: ['data'],
+    theme: { primary: 'black', secondary: '' },
+    logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/logo-amazon.svg"
+  },
+    snowflake: {
+    datamappingUrl: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/SNOW.US/SNOW.US_metrics_mapping.json',
+    tabMenu: ['data'],
+    theme: { primary: 'black', secondary: '' },
+    logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/snowflakelong.svg"
+  },
+    celsius: {
+    datamappingUrl: 'https://pulse-stockprice.s3.us-east-2.amazonaws.com/CELH.US/CELH.US_metrics_mapping.json',
+    tabMenu: ['data'],
+    theme: { primary: 'black', secondary: '' },
+    logo: "https://pulse-stockprice.s3.us-east-2.amazonaws.com/Logos/celsiuslong.svg"
+  },
+    default: {
     datamappingUrl: 'datamapping.json',
     tabMenu: ['sankey', 'data'],
     theme: { primary: 'black', secondary: '' },
